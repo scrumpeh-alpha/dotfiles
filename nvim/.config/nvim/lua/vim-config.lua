12a6da98e2974b3c1698bac3090e55ebdf42a79d
scrumpeh-alpha <gowshikrajan690@gmail.com>
2024-11-04 18:47:06 +0300

    add extra keybinds in vim-config

--- nvim/.config/nvim/lua/vim-config.lua
+++ nvim/.config/nvim/lua/vim-config.lua
@@ -5,3 +5,12 @@ vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set number")
 
+-- clipboard
+vim.cmd("set clipboard+=unnamedplus")
+
+-- keybindings
+vim.keymap.set("n", "<C-h>", "<C-w>h", {})
+vim.keymap.set("n", "<C-l>", "<C-w>l", {})
+vim.keymap.set("n", "<C-j>", "<C-w>j", {})
+vim.keymap.set("n", "<C-k>", "<C-w>k", {})
+vim.keymap.set("n", "<Esc>", "<cmd> noh <CR>", {})
