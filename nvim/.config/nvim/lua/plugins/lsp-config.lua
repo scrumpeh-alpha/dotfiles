30d31928c3a08dd7834515158460990a1451c1ca
scrumpeh-alpha <gowshikrajan690@gmail.com>
2025-05-30 13:04:17 +0300

    update nvim lspconfig. see file

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -19,44 +19,27 @@ return {
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local lspconfig = require("lspconfig")
+			-- Updated to new standard
+			-- Old syntax resulted in LSP launching twice
 
-			--Config lsp for each lang here as follows
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.ruff.setup({
-				capabilities = capabilities,
-			})
-			-- lspconfig.pylsp.setup({
-			-- 	capabilities = capabilities,
-			-- })
-			-- lspconfig.clangd.setup({
-			-- 	capabilities = capabilities,
-			-- })
-			lspconfig.texlab.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.cssls.setup({
-				capabilities = capabilities,
-			})
-			-- lspconfig.html.setup({
-			-- 	capabilities = capabilities,
-			-- })
-            lspconfig.emmet_ls.setup({
-                capabilities=capabilities,
-            })
+			local languages = {
+				"lua_ls",
+				"ruff",
+				"pylsp",
+				"pyright",
+				"clangd",
+				"texlab",
+				"cssls",
+				"html",
+				"ts_ls",
+				"emmet_ls",
+			}
+            -- Enable all languages in list
+			vim.lsp.enable(languages)
 
-			-- lspconfig.pyright.setup({
-			-- 	capabilities = capabilities,
-			-- })
-            lspconfig.ts_ls.setup({
-                capabilities=capabilities,
-            })
-
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Show LSP Info" })
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Show LSP Info" })
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
-			vim.keymap.set({"n", "v"}, "<leader>ca", vim.lsp.buf.code_action, { desc = "Show code actions" })
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Show code actions" })
 			vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, { desc = "Rename variable" })
 		end,
 	},
