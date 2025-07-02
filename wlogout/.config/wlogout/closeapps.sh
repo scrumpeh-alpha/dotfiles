005755394cc87bb395775c33d763487d38b05b1f
scrumpeh-alpha <gowshikrajan690@gmail.com>
2025-07-02 19:25:43 +0300

    add wlogout; note script for closing programs gracefully, TODO

---
+++ wlogout/.config/wlogout/closeapps.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+notify-send "wlogout" "Initiating logout"
+
+sleep 1
+
+HYPRCMDS=$(hyprctl -j clients | jq -j '.[] | "dispatch closewindow address:\(.address); "')
+hyprctl --batch "$HYPRCMDS" >> /tmp/hypr/hyprexitwithgrace.log 2>&1
+
+notify-send "wlogout" "Closing Applications..."
+
+sleep 2
+
+COUNT=$(hyprctl clients | grep "class:" | wc -l)
+if [ "$COUNT" -eq "0" ]; then
+    notify-send "wlogout" "Closed Applications."
+else
+    notify-send "wlogout" "Some apps didn't close. Not shutting down."
+    return
+fi
