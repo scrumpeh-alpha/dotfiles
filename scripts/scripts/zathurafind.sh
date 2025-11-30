05afb0c41c0844af95da5e79a455b5a9602611b1
scrumpeh-alpha <gowshikrajan690@gmail.com>
2025-11-30 18:34:02 +0300

    add zathurafind; move nohup output

--- scripts/scripts/zathurafind.sh
+++ scripts/scripts/zathurafind.sh
@@ -1,11 +1,8 @@
 #/bin/bash
 
-cd $BOOKS_DIR
 bn="$(find . -type f | fzf)"
 
 echo $bn
 if [[ $bn != "" ]]; then
     nohup zathura "${bn}" >/dev/null 2>&1 & disown
-    # nohup zathura "${bn}" & disown
 fi
-cd $OLDPWD
