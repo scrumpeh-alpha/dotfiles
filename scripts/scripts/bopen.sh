7f3f0b56fc667ea60ca350c87bcd3249af5a3761
scrumpeh-alpha <gowshikrajan690@gmail.com>
2025-12-08 19:57:07 +0300

    add custom programs argument

--- scripts/scripts/bopen.sh
+++ scripts/scripts/bopen.sh
@@ -1,13 +1,17 @@
 #/bin/bash
 
+VIEWER=zathura
+if [[ $1 ]]; then
+    VIEWER=$1
+fi
+
 (
     cd "$BOOKS_DIR" || exit 1
     bn="$(find . -type f | fzf)"
 
-    echo $bn
-    if [[ $bn != "" ]]; then
-        nohup zathura "${bn}" >/dev/null 2>&1 &
+    if [[ -n "$bn" ]]; then
+        echo "Opening $bn with $VIEWER"
+        nohup "$VIEWER" "${bn}" >/dev/null 2>&1 &
         # nohup zathura "${bn}" & disown
     fi
 )
-# cd $OLDPWD
